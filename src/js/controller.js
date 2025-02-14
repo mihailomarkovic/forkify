0e80d9bc29d316e97bbf91b7dea515f4b783f39c
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-02-14 13:05:56 +0100

    Add listening for load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,11 +30,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -158,4 +162,5 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
