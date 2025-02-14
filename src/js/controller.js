044af904147402dfe56159f66e877ef544950a79
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-02-14 15:03:27 +0100

    Add timeout helper function for error handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,19 +6,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// NEW API URL (instead of the one shown in the video)
-// https://forkify-api.jonas.io
-
-///////////////////////////////////////
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
