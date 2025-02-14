2b059bb1e2ecc513c7c98162b1881936acfd995f
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-02-14 13:46:04 +0100

    Create model for loading recipe function

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import * as model from './model.js';
+
 import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -33,28 +35,11 @@ const showRecipe = async function () {
     const id = window.location.hash.slice(1);
 
     if (!id) return;
-
-    // 1) Loading recipe
     renderSpinner(recipeContainer);
 
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
     console.log(recipe);
 
     // 2) Rendering recipe
