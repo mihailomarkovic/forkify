6f7f41303cc74210ea6553cac0a6e44651d77833
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-02-15 15:12:55 +0100

    Add JSDocs comment

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,6 +2,13 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
+
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string insted of rendering to the DOM
+   * @returns{undefined | string} A markup string is returned if render=false
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
